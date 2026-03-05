ae8b85151ac5bec1cfa71d6a9f6ab2c8198a33ae
salahkhafaga1 <salahkhafaga1234@gmail.com>
2026-03-05 17:48:10 +0200

    Refactor code structure for improved readability and maintainability

--- ML/ML_project/project_1/visuals.py
+++ ML/ML_project/project_1/visuals.py
@@ -2,30 +2,33 @@
 # Suppress matplotlib user warnings
 # Necessary for newer version of matplotlib
 import warnings
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pl
+warnings.filterwarnings("ignore", category = UserWarning, module = "matplotlib")
+
 import numpy as np
 from sklearn.model_selection import learning_curve, ShuffleSplit, train_test_split
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import learning_curve, validation_curve
 
 # Suppress matplotlib user warnings
 warnings.filterwarnings("ignore", category=UserWarning, module="matplotlib")
 
 # Display inline matplotlib plots with IPython
 from IPython import get_ipython
-get_ipython().run_line_magic('matplotlib', 'inline')
+# get_ipython().run_line_magic('matplotlib', 'inline')
 
 def ModelComplexity(X, y):
     """ Calculates the performance of the model as model complexity increases.
         The learning and testing errors rates are then plotted. """
     
     # Create 10 cross-validation sets for training and testing
-    cv = ShuffleSplit(X.shape[0], n_iter = 10, test_size = 0.2, random_state = 0)
+    cv = ShuffleSplit(n_splits = 10, test_size = 0.2, random_state = 0)
 
     # Vary the max_depth parameter from 1 to 10
     max_depth = np.arange(1,11)
 
     # Calculate the training and testing scores
-    train_scores, test_scores = curves.validation_curve(DecisionTreeRegressor(), X, y, \
+    train_scores, test_scores = validation_curve(DecisionTreeRegressor(), X, y, \
         param_name = "max_depth", param_range = max_depth, cv = cv, scoring = 'r2')
 
     # Find the mean and standard deviation for smoothing
